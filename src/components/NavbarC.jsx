8bfbc00c7eed46f3686a708867cee403fd16b023
Musul Man <musulmanco@gmail.com>
2024-09-09 21:12:23 -0300

    se actualiza navbar y se crean 2 visualizaciones nuevas

--- src/components/NavbarC.jsx
+++ src/components/NavbarC.jsx
@@ -1,35 +1,103 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const NavbarC = () => {
+  const navigate = useNavigate();
+  const token = JSON.parse(sessionStorage.getItem("token")) || null;
+  const rol = JSON.parse(sessionStorage.getItem("rol")) || null;
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("rol");
+
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid className="navbar-fondo">
-          <Navbar.Brand href="#home">
-            <img src="logogym.png" alt="" width={80} />
-          </Navbar.Brand>
+        <Container>
+          <NavLink
+            to={
+              token && rol === "admin"
+                ? "/admin-inicio"
+                : token && rol === "usuario"
+                ? "/usuario-inicio"
+                : "/"
+            }
+            className="nav-link"
+          >
+            Logo
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className="btn-navbar" href="#home">
+              <NavLink
+                to={
+                  rol === "admin"
+                    ? "/admin-inicio"
+                    : rol === "usuario"
+                    ? "/usuario-inicio"
+                    : "/"
+                }
+                className="nav-link"
+              >
                 Inicio
-              </Nav.Link>
-              <Nav.Link className="btn-navbar" href="#link">
-                Sobre Nosotros
-              </Nav.Link>
-              <Nav.Link className="btn-navbar" href="#link">
-                Contacto
-              </Nav.Link>
+              </NavLink>
+              {rol === "usuario" ||
+                (!token && (
+                  <>
+                    <NavLink to="/sobre-nosotros" className="nav-link">
+                      Sobre Nosotros
+                    </NavLink>
+                    <NavLink to="/contacto" className="nav-link">
+                      Contacto
+                    </NavLink>
+                  </>
+                ))}
+              {rol === "admin" && (
+                <>
+                  <NavLink to="/admin-usuarios" className="nav-link">
+                    Panel de Usuarios
+                  </NavLink>
+                  <NavLink to="/admin-productos" className="nav-link">
+                    Panel de Productos
+                  </NavLink>{" "}
+                  <NavLink to="/" className="nav-link">
+                    Ir a vista Usuario
+                  </NavLink>
+                </>
+              )}
+              {rol === "usuario" && (
+                <>
+                  <NavLink to="/usuario-favoritos" className="nav-link">
+                    Favoritos
+                  </NavLink>
+                  <NavLink to="/usuario-carrito" className="nav-link">
+                    Carrito
+                  </NavLink>
+                </>
+              )}
             </Nav>
+
             <Nav className="ms-auto">
-              <Nav.Link className="btn-navbar" href="#home">
-                Iniciar Sesión
-              </Nav.Link>
-              <Nav.Link className="btn-navbar" href="#link">
-                Registrarse
-              </Nav.Link>
+              {token ? (
+                <NavLink to="#" className="nav-link" onClick={handleLogout}>
+                  Cerrar Sesion
+                </NavLink>
+              ) : (
+                <>
+                  <NavLink to="/iniciar-sesion" className="nav-link">
+                    Iniciar Sesion
+                  </NavLink>
+                  <NavLink to="/registrarse" className="nav-link">
+                    Registrarse
+                  </NavLink>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ src/pages/TurnosPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TurnosPage = () => {
+  return <div>TurnosPage</div>;
+};
+
+export default TurnosPage;
